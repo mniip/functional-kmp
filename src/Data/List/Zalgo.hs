6d907752979f1b3fe66b987a4bdc845486f986bd
mniip <mniip@mniip.com>
2015-08-29 06:18:54 +0300

    Add haddocks

--- src/Data/List/Zalgo.hs
+++ src/Data/List/Zalgo.hs
@@ -1,20 +1,47 @@
 module Data.List.Zalgo
-	(
-		zFun,
-		isInfixOf,
-		indexOf,
+    (
+        zFun,
+        isInfixOf,
+        indexOf,
 
-		zFunBy,
-		isInfixBy,
-		indexBy,
+        zFunBy,
+        isInfixBy,
+        indexBy,
 
-		genericZFun,
-		genericIndexOf,
+        genericZFun,
+        genericIndexOf,
 
-		genericZFunBy,
-		genericIndexBy
-	)
-	where
+        genericZFunBy,
+        genericIndexBy
+    )
+    where
+--------------------------------------------------------------------------------
+-- |
+-- Module: Data.List.Zalgo
+-- Copyright: (C) 2015 mniip
+-- Maintainer: mniip <mniip@mniip.com>
+-- License: BSD3
+-- Portability: portable
+-- Stability: experimental
+--
+-- A few efficient list-processing functions using the Z-function, which is
+-- defined as:
+--
+-- > (z xs) !! i
+--
+-- is the length of the largest proper substring of @xs@ ending at position @i@,
+-- such that it equals the beginning of @xs@.
+--
+-- For example:
+--
+-- > .-----.             .-----.
+-- > a b a c a b a a a b a b a c d
+-- > 0 0 1 0 1 2 3 1 1 2 3 2 3 4 0
+-- >                           ^
+--
+-- The marked substrings are equal, hence the value at the marked location is
+-- their length, 4.
+--------------------------------------------------------------------------------
 
 import Data.List hiding (isInfixOf)
 import Data.Maybe
@@ -23,60 +50,87 @@ import Data.List.Zalgo.Internal
 joinLists :: [a] -> [a] -> [Maybe a]
 joinLists n h = map Just n ++ Nothing:map Just h
 
+-- | _O(N)._ Compute the Z-function for a list.
 zFun :: Eq a => [a] -> [Int]
 zFun xs = map zLength $ zTraverse xs
 
+-- | _O(N+H)._ @isInfixOf needle haystack@ tests whether needle is fully
+-- contained somewhere in haystack.
 isInfixOf :: Eq a => [a] -> [a] -> Bool
 isInfixOf n h = isJust $ indexOf n h
 
+-- | _O(N+H)._ @indexOf needle haystack@ returns the index at which needle
+-- is found in haystack, or Nothing if it's not.
 indexOf :: Eq a => [a] -> [a] -> Maybe Int
 indexOf n h = go $ zFun $ joinLists n h
-	where
-		ln = length n
-		go [] = Nothing
-		go (l:ls)
-			| l == ln = Just (-l-l)
-			| otherwise = fmap (+ 1) $ go ls
+    where
+        ln = length n
+        go [] = Nothing
+        go (l:ls)
+            | l == ln = Just (-l-l)
+            | otherwise = fmap (+ 1) $ go ls
 
+-- | == Custom predicates
+-- The '...By' set of functions takes a custom equality predicate, and due to
+-- the optimized nature of the algorithm, passed predicate must conform to
+-- some laws:
+-- 
+-- > Commutativity: a == b  =>  b == a
+-- > Inverse commutativity: a /= b  =>  b /= a
+-- > Transitivity: a == b and b == c  =>  a == c
+-- > Inverse transitivity: a == b and b /= c  =>  a /= c
+--
+-- If these laws do not hold, the behavior is undefined.
+
+-- | _O(N) and O(N) calls to the predicate._ Compute the Z-function using a
+-- custom equality predicate.
 zFunBy :: (a -> a -> Bool) -> [a] -> [Int]
 zFunBy eq xs = map zLength $ zTraverseBy eq xs
 
+-- | _O(N+H) and O(N+H) calls to the predicate._ Compute 'isInfixOf' using a
+-- custom equality predicate.
 isInfixBy :: (a -> a -> Bool) -> [a] -> [a] -> Bool
 isInfixBy eq n h = isJust $ indexBy eq n h
 
+-- | _O(N+H) and O(N+H) calls to the predicate._ Compute 'indexOf' using a
+-- cusom equality predicate.
 indexBy :: (a -> a -> Bool) -> [a] -> [a] -> Maybe Int
 indexBy eq n h = go $ zFunBy (maybeEq eq) $ joinLists n h
-	where
-		ln = length n
-		go [] = Nothing
-		go (l:ls)
-			| l == ln = Just (-l-l)
-			| otherwise = fmap (+ 1) $ go ls
-		maybeEq eq (Just x) (Just y) = x `eq` y
-		maybeEq _ _ _ = False
+    where
+        ln = length n
+        go [] = Nothing
+        go (l:ls)
+            | l == ln = Just (-l-l)
+            | otherwise = fmap (+ 1) $ go ls
+        maybeEq eq (Just x) (Just y) = x `eq` y
+        maybeEq _ _ _ = False
+
+-- | == Generic functions
+-- Some of the functions are generalized over the type of the numbers they
+-- return, but keep in mind that the amount of arithmetic operations is linear.
 
 genericZFun :: (Num i, Eq a) => [a] -> [i]
 genericZFun xs = map (fromMaybe 0 . gzLength) $ gzTraverse xs
 
 genericIndexOf :: (Eq i, Num i, Eq a) => [a] -> [a] -> Maybe i
 genericIndexOf n h = go $ genericZFun $ joinLists n h
-	where
-		ln = genericLength n
-		go [] = Nothing
-		go (l:ls)
-			| l == ln = Just (-l-l)
-			| otherwise = fmap (+ 1) $ go ls
+    where
+        ln = genericLength n
+        go [] = Nothing
+        go (l:ls)
+            | l == ln = Just (-l-l)
+            | otherwise = fmap (+ 1) $ go ls
 
 genericZFunBy :: Num i => (a -> a -> Bool) -> [a] -> [i]
 genericZFunBy eq xs = map (fromMaybe 0 . gzLength) $ gzTraverseBy eq xs
 
 genericIndexBy :: (Eq i, Num i) => (a -> a -> Bool) -> [a] -> [a] -> Maybe i
 genericIndexBy eq n h = go $ genericZFunBy (maybeEq eq) $ joinLists n h
-	where
-		ln = genericLength n
-		go [] = Nothing
-		go (l:ls)
-			| l == ln = Just (-l-l)
-			| otherwise = fmap (+ 1) $ go ls
-		maybeEq eq (Just x) (Just y) = x `eq` y
-		maybeEq _ _ _ = False
+    where
+        ln = genericLength n
+        go [] = Nothing
+        go (l:ls)
+            | l == ln = Just (-l-l)
+            | otherwise = fmap (+ 1) $ go ls
+        maybeEq eq (Just x) (Just y) = x `eq` y
+        maybeEq _ _ _ = False
--- src/Data/List/Zalgo/Internal.hs
+++ src/Data/List/Zalgo/Internal.hs
@@ -1,84 +1,117 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module: Data.List.Zalgo.Internal
+-- Copyright: (C) 2015 mniip
+-- Maintainer: mniip <mniip@mniip.com>
+-- License: BSD3
+-- Portability: portable
+-- Stability: experimental
+--
+-- Implementation of the Z-function on cons-cells.
+--
+-- Z-function has a simple implementation using arrays, the challenge, however
+-- was to implement it on haskell lists (which are cons cells) without losing
+-- any complexity. The following code uses a tying-the-know data passing
+-- structure, however no complicated laziness mechanisms are used: a 'ZState'
+-- only depends on previous 'ZState's and therefore this algorithm can be safely
+-- implemented in a strict language with pointers.
+--------------------------------------------------------------------------------
+
 module Data.List.Zalgo.Internal
-	(
-		ZState(..),
-		GenericZState(..),
-		zTraverse,
-		zTraverseBy,
-		gzTraverse,
-		gzTraverseBy
-	)
-	where
+    (
+        ZState(..),
+        GenericZState(..),
+        zTraverse,
+        zTraverseBy,
+        gzTraverse,
+        gzTraverseBy
+    )
+    where
 
 import Data.Maybe
 
+-- | A state of the Z-function computation. 'zTail' points to the tail of the
+-- input at the state's position, 'zLength' is the value of the Z-function, and
+-- 'zPrev' is a reference to the list of 'ZState's starting from position
+-- described by 'zLength'.
 data ZState a = ZState { zTail :: [a], zLength :: !Int, zPrev :: [ZState a] }
 
+-- | _O(N)._ Compute the list of Z-function states for a given input.
 zTraverse :: Eq a => [a] -> [ZState a]
 zTraverse [] = []
 zTraverse xw@(_:xs) = let
-		pz = ZState{zTail = xw, zLength = 0, zPrev = pzs}
-		z = ZState{zTail = xs, zLength = 0, zPrev = pzs}
-		pzs = pz:zs
-		zs = z:go z xs
-	in zs
-	where
-		go _ [] = []
-		go pz (x:xs) = let z = recurse pz x xs
-			in z:go z xs
-		recurse ZState{zLength = zl, zPrev = zpw@(zp@ZState{zTail = zpt:_}:zps)} x xs
-			| zpt == x = ZState{zTail = xs, zLength = zl + 1, zPrev = zps}
-			| zl == 0 = ZState{zTail = xs, zLength = 0, zPrev = zpw}
-			| otherwise = recurse zp x xs
+        pz = ZState{zTail = xw, zLength = 0, zPrev = pzs}
+        z = ZState{zTail = xs, zLength = 0, zPrev = pzs}
+        pzs = pz:zs
+        zs = z:go z xs
+    in zs
+    where
+        go _ [] = []
+        go pz (x:xs) = let z = recurse pz x xs
+            in z:go z xs
+        recurse ZState{zLength = zl, zPrev = zpw@(zp@ZState{zTail = zpt:_}:zps)} x xs
+            | zpt == x = ZState{zTail = xs, zLength = zl + 1, zPrev = zps}
+            | zl == 0 = ZState{zTail = xs, zLength = 0, zPrev = zpw}
+            | otherwise = recurse zp x xs
 
+-- | _O(N) and O(N) calls to the predicate._ Compute the list of Z-function
+-- states using a given equality predicate. See "Data.List.Zalgo.zFunBy" for a
+-- detailed explanation of what predicates are allowed.
 zTraverseBy :: (a -> a -> Bool) -> [a] -> [ZState a]
 zTraverseBy eq [] = []
 zTraverseBy eq xw@(_:xs) = let
-		pz = ZState{zTail = xw, zLength = 0, zPrev = pzs}
-		z = ZState{zTail = xs, zLength = 0, zPrev = pzs}
-		pzs = pz:zs
-		zs = z:go eq z xs
-	in zs
-	where
-		go _ _ [] = []
-		go eq pz (x:xs) = let z = recurse eq pz x xs
-			in z:go eq z xs
-		recurse eq ZState{zLength = zl, zPrev = zpw@(zp@ZState{zTail = zpt:_}:zps)} x xs
-			| zpt `eq` x = ZState{zTail = xs, zLength = zl + 1, zPrev = zps}
-			| zl == 0 = ZState{zTail = xs, zLength = 0, zPrev = zpw}
-			| otherwise = recurse eq zp x xs
+        pz = ZState{zTail = xw, zLength = 0, zPrev = pzs}
+        z = ZState{zTail = xs, zLength = 0, zPrev = pzs}
+        pzs = pz:zs
+        zs = z:go eq z xs
+    in zs
+    where
+        go _ _ [] = []
+        go eq pz (x:xs) = let z = recurse eq pz x xs
+            in z:go eq z xs
+        recurse eq ZState{zLength = zl, zPrev = zpw@(zp@ZState{zTail = zpt:_}:zps)} x xs
+            | zpt `eq` x = ZState{zTail = xs, zLength = zl + 1, zPrev = zps}
+            | zl == 0 = ZState{zTail = xs, zLength = 0, zPrev = zpw}
+            | otherwise = recurse eq zp x xs
 
+-- 'ZState' generalized of the number type. 'Nothing' represents zero and is
+-- used to avoid the 'Eq' constraint
 data GenericZState i a = GenericZState { gzTail :: [a], gzLength :: Maybe i, gzPrev :: [GenericZState i a] }
 
+-- | _O(N) and O(N) plus-1's._ Compute the list of Z-function states using a
+-- given number type.
 gzTraverse :: (Num i, Eq a) => [a] -> [GenericZState i a]
 gzTraverse [] = []
 gzTraverse xw@(_:xs) = let
-		pz = GenericZState{gzTail = xw, gzLength = Nothing, gzPrev = pzs}
-		z = GenericZState{gzTail = xs, gzLength = Nothing, gzPrev = pzs}
-		pzs = pz:zs
-		zs = z:go z xs
-	in zs
-	where
-		go _ [] = []
-		go pz (x:xs) = let z = recurse pz x xs
-			in z:go z xs
-		recurse GenericZState{gzLength = zl, gzPrev = zpw@(zp@GenericZState{gzTail = zpt:_}:zps)} x xs
-			| zpt == x = GenericZState{gzTail = xs, gzLength = Just $ maybe 1 (+ 1) zl, gzPrev = zps}
-			| isNothing zl = GenericZState{gzTail = xs, gzLength = Nothing, gzPrev = zpw}
-			| otherwise = recurse zp x xs
+        pz = GenericZState{gzTail = xw, gzLength = Nothing, gzPrev = pzs}
+        z = GenericZState{gzTail = xs, gzLength = Nothing, gzPrev = pzs}
+        pzs = pz:zs
+        zs = z:go z xs
+    in zs
+    where
+        go _ [] = []
+        go pz (x:xs) = let z = recurse pz x xs
+            in z:go z xs
+        recurse GenericZState{gzLength = zl, gzPrev = zpw@(zp@GenericZState{gzTail = zpt:_}:zps)} x xs
+            | zpt == x = GenericZState{gzTail = xs, gzLength = Just $ maybe 1 (+ 1) zl, gzPrev = zps}
+            | isNothing zl = GenericZState{gzTail = xs, gzLength = Nothing, gzPrev = zpw}
+            | otherwise = recurse zp x xs
 
+-- | _O(N), O(N) calls to the predicate, and O(N) plus-1's._ Compute the list of
+-- Z-function states using a given number type and a given equality predicate.
 gzTraverseBy :: Num i => (a -> a -> Bool) -> [a] -> [GenericZState i a]
 gzTraverseBy eq [] = []
 gzTraverseBy eq xw@(_:xs) = let
-		pz = GenericZState{gzTail = xw, gzLength = Nothing, gzPrev = pzs}
-		z = GenericZState{gzTail = xs, gzLength = Nothing, gzPrev = pzs}
-		pzs = pz:zs
-		zs = z:go eq z xs
-	in zs
-	where
-		go _ _ [] = []
-		go eq pz (x:xs) = let z = recurse eq pz x xs
-			in z:go eq z xs
-		recurse eq GenericZState{gzLength = zl, gzPrev = zpw@(zp@GenericZState{gzTail = zpt:_}:zps)} x xs
-			| zpt `eq` x = GenericZState{gzTail = xs, gzLength = Just $ maybe 1 (+ 1) zl, gzPrev = zps}
-			| isNothing zl = GenericZState{gzTail = xs, gzLength = Nothing, gzPrev = zpw}
-			| otherwise = recurse eq zp x xs
+        pz = GenericZState{gzTail = xw, gzLength = Nothing, gzPrev = pzs}
+        z = GenericZState{gzTail = xs, gzLength = Nothing, gzPrev = pzs}
+        pzs = pz:zs
+        zs = z:go eq z xs
+    in zs
+    where
+        go _ _ [] = []
+        go eq pz (x:xs) = let z = recurse eq pz x xs
+            in z:go eq z xs
+        recurse eq GenericZState{gzLength = zl, gzPrev = zpw@(zp@GenericZState{gzTail = zpt:_}:zps)} x xs
+            | zpt `eq` x = GenericZState{gzTail = xs, gzLength = Just $ maybe 1 (+ 1) zl, gzPrev = zps}
+            | isNothing zl = GenericZState{gzTail = xs, gzLength = Nothing, gzPrev = zpw}
+            | otherwise = recurse eq zp x xs
